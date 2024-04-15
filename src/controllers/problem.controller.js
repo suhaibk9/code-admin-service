f624fea90b48bd0873c0dc9c4a69c00f636da18a
Suhaib Khan <suhaib0900@gmail.com>
2024-04-15 11:53:58 +0100

    Added Error Handling

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,37 +1,48 @@
 const { StatusCodes } = require('http-status-codes');
+const NotImplemented = require('../errors/NotImplemented.error');
 //Ping Problem Controller
 function pingProblemController(req, res) {
   return res.json({ Status: 'Problem Controller is Alive' });
 }
 //Create a new problem
-function addProblem(req, res) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: 'Not Implemented' });
+function addProblem(req, res, next) {
+  try {
+    throw new NotImplemented('addProblem');
+  } catch (err) {
+    next(err);
+  }
 }
 //Get Problem
-function getProblem(req, res) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: 'Not Implemented' });
+function getProblem(req, res, next) {
+  try {
+    throw new NotImplemented('getProblem');
+  } catch (err) {
+    next(err);
+  }
 }
 //Get All Problems based on the filters
-function getProblems(req, res) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: 'Not Implemented' });
+function getProblems(req, res, next) {
+  try {
+    throw new NotImplemented('getProblems');
+  } catch (err) {
+    next(err);
+  }
 }
 //Update Problem
-function updateProblem(req, res) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: 'Not Implemented' });
+function updateProblem(req, res, next) {
+  try {
+    throw new NotImplemented('updateProblem');
+  } catch (err) {
+    next(err);
+  }
 }
 //Delete Problem
-function deleteProblem(req, res) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: 'Not Implemented' });
+function deleteProblem(req, res, next) {
+  try {
+    throw new NotImplemented('deleteProblem');
+  } catch (err) {
+    next(err);
+  }
 }
 module.exports = {
   pingProblemController,
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { PORT } = require('../src/config/server.config');
 const app = express();
 const bodyParser = require('body-parser');
+const BaseError = require('./errors/Base.error');
 const apiRouter = require('./routes/index');
+const errorHandler = require('./utils/errorHandler');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -10,6 +12,7 @@ app.get('/ping', (req, res) => {
   return res.json({ Status: 'Server is Alive' });
 });
 app.use('/api', apiRouter);
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
