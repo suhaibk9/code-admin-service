70e6f6f506aa5be08d09119a3806e9498bba4c7d
Suhaib Khan <suhaib0900@gmail.com>
2024-04-15 02:11:06 +0100

    Implemented Not Implemented in Controller

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,17 +1,38 @@
+const { StatusCodes } = require('http-status-codes');
 //Ping Problem Controller
 function pingProblemController(req, res) {
   return res.json({ Status: 'Problem Controller is Alive' });
 }
 //Create a new problem
-function addProblem(req, res) {}
+function addProblem(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: 'Not Implemented' });
+}
 //Get Problem
-function getProblem(req, res) {}
+function getProblem(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: 'Not Implemented' });
+}
 //Get All Problems based on the filters
-function getProblems(req, res) {}
+function getProblems(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: 'Not Implemented' });
+}
 //Update Problem
-function updateProblem(req, res) {}
+function updateProblem(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: 'Not Implemented' });
+}
 //Delete Problem
-function deleteProblem(req, res) {}
+function deleteProblem(req, res) {
+  return res
+    .status(StatusCodes.NOT_IMPLEMENTED)
+    .json({ message: 'Not Implemented' });
+}
 module.exports = {
   pingProblemController,
   addProblem,
